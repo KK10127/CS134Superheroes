d48e799d106296a7a7fb5bcad526b6a1f523061d
Jay Montoya <jayjoplin9898@gmail.com>
2019-03-28 14:40:22 -0700

    added Student class and JSONLoader

---
+++ app/src/main/java/edu/miracosta/cs134/model/JSONLoader.java
@@ -0,0 +1,65 @@
+package edu.miracosta.cs134.model;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class loads the Student data from a formatted JSON file.
+ * Populates data model (Student) with data.
+ */
+public class JSONLoader {
+
+    /**
+     * Loads JSON data from a file in the assets directory.
+     *
+     */
+    public static List<Student> loadJSONFromAsset(Context context) throws IOException {
+        List<Student> allStudentsList = new ArrayList<>();
+        String json = null;
+        InputStream is = context.getAssets().open("cs134superheroes.json");
+        int size = is.available();
+        byte[] buffer = new byte[size];
+        is.read(buffer);
+        is.close();
+        json = new String(buffer, "UTF-8");
+
+        try {
+            JSONObject jsonRootObject = new JSONObject(json);
+            JSONArray allStudentsJSON = jsonRootObject.getJSONArray("Students");
+
+            // Loop through all the students in the JSON data, create a student
+            int length = allStudentsJSON.length();
+            JSONObject studentJSON;
+            String fileName, name, superPower, oneThing;
+            Student student;
+
+            for (int i = 0; i < length;  i++) {
+                studentJSON = allStudentsJSON.getJSONObject(i);
+                // extract the info
+                fileName = studentJSON.getString("FileName");
+                name = studentJSON.getString("Name");
+                superPower = studentJSON.getString("SuperPower");
+                oneThing = studentJSON.getString("OneThing");
+
+                // create object and add to the list
+                student = new Student(fileName, name, superPower, oneThing);
+                allStudentsList.add(student);
+            }
+        } catch (JSONException e) {
+            Log.e("Superheroes Quiz", e.getMessage());
+        }
+
+        // return the list
+        return allStudentsList;
+    }
+
+}
---
+++ app/src/main/java/edu/miracosta/cs134/model/Student.java
@@ -0,0 +1,74 @@
+package edu.miracosta.cs134.model;
+
+import java.util.Objects;
+
+/**
+ * Represents a Student for the purposes of the Superheroe quiz, including the fileName, name,
+ * superPower, and oneThing.
+ *
+ * @author Jay Montoya
+ * @version 1.0
+ */
+public class Student {
+
+    private String mFileName;
+    private String mName;
+    private String mSuperPower;
+    private String mOneThing;
+
+    /**
+     * Instatiates a new <code>Student</code> given its name and region.
+     * @param fileName
+     * @param name
+     * @param superPower
+     * @param oneThing
+     */
+    public Student(String fileName, String name, String superPower, String oneThing) {
+        mFileName = name;
+        mName = name;
+        mSuperPower = superPower;
+        mOneThing = oneThing;
+    }
+
+    public String getFileName() {
+        return mFileName;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getSuperPower() {
+        return mSuperPower;
+    }
+
+    public String getOneThing() {
+        return mOneThing;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "mFileName='" + mFileName + '\'' +
+                ", mName='" + mName + '\'' +
+                ", mSuperPower='" + mSuperPower + '\'' +
+                ", mOneThing='" + mOneThing + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(mFileName, student.mFileName) &&
+                Objects.equals(mName, student.mName) &&
+                Objects.equals(mSuperPower, student.mSuperPower) &&
+                Objects.equals(mOneThing, student.mOneThing);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mFileName, mName, mSuperPower, mOneThing);
+    }
+}
